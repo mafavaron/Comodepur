d1b749c5588a51751a50aaabf64ce2c9f50e5398
Mauri Favaron <mafavaron@mac.com>
2019-05-30 12:33:59 +0200

    Update met_compare.r
    Comparison plot added for wind direction

--- src/met_compare.r
+++ src/met_compare.r
@@ -13,11 +13,22 @@ get.conv.data <- function() {
 }
 
 compare <- function() {
+  
+  # Get data
   s <- get.sonic.data()
   c <- get.conv.data()
+  
+  # Wind speed
   vel.max <- max(c(c$Vel, s$Vel))
   png(file="compare/Vel.png", height=600, width=600)
   plot(c$Vel, s$Vel, cex=0.2, xlab="Conventional",ylab="Sonic", main="Vel (m/s)", xlim=c(0,vel.max), ylim=c(0,vel.max))
   abline(0, 1, lwd=2, col="light blue")
   dev.off()
+  
+  # Wind direction
+  png(file="compare/Dir.png", height=600, width=600)
+  plot(c$Dir, s$Dir, cex=0.2, xlab="Conventional",ylab="Sonic", main="Dir (° from North)", xlim=c(0,360), ylim=c(0,360))
+  abline(0, 1, lwd=2, col="light blue")
+  dev.off()
+  
 }
