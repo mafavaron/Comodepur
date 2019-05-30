b857bd4e234bbf37919c4bc54555888b932a4814
Mauri Favaron <mafavaron@mac.com>
2019-05-30 10:48:16 +0200

    Update met_compare.r
    Wind speed comparison plot adapted for publication

--- src/met_compare.r
+++ src/met_compare.r
@@ -15,8 +15,9 @@ get.conv.data <- function() {
 compare <- function() {
   s <- get.sonic.data()
   c <- get.conv.data()
+  vel.max <- max(c(c$Vel, s$Vel))
   png(file="compare/Vel.png", height=600, width=600)
-  plot(c$Vel, s$Vel, cex=0.2,xlab="Conventional",ylab="Sonic", main="Vel (m/s)", xlab="Conventional", ylab="Sonic", main="Vel (m/s)")
-  abline(lm(s$Vel~c$Vel+1), lwd=2, col="light blue")
+  plot(c$Vel, s$Vel, cex=0.2, xlab="Conventional",ylab="Sonic", main="Vel (m/s)", xlim=c(0,vel.max), ylim=c(0,vel.max))
+  abline(0, 1, lwd=2, col="light blue")
   dev.off()
 }
