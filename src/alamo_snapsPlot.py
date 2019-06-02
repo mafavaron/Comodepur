a4b63c43f9a2ec1aa88e366eef07d0d9b92a8ada
Mauri Favaron <mafavaron@mac.com>
2019-06-02 21:54:11 +0200

    Create alamo_snapsPlot.py
    Revised version, supporting binary data files as fro ALAMO new version.

---
+++ src/alamo_snapsPlot.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+
+	# Check input parameters
+	if len(sys.argv) != 3:
+		print("alamo_snapsPlot.py:: error: Invalid argument list")
+		print(" ")
+		print("Usage:")
+		print(" ")
+		print("  ./alamo_snapsPlot.py <snaps_path> <images_path>")
+		print(" ")
+		print("by: Mauri Favaron - 2018")
+		sys.exit(1)
+	snapsPath  = sys.argv[1]
+	imagesPath = sys.argv[2]
+	
+	# Find snaps in snaps path
+	snaps = glob.glob(os.path.join(snapsPath, "snap_*.bin"))
+	if len(snaps) <= 0:
+		print("alamo_snapsPlot.py:: error: Snaps path contains no snaps")
+		sys.exit(2)
+	try:
+		guideFile = os.path.join(snapsPath, "guide.txt")
+		f = open(guideFile, "r")
+		lines = f.readlines()
+		f.close()
+	except:
+		print("alamo_snapsPlot.py:: error: No or invalid guide file")
+		sys.exit(3)
+	if len(lines) <= 0:
+		print("alamo_snapsPlot.py:: error: No or invalid guide file")
+		sys.exit(3)
+		
+	# Get plotting extrema from the guide file
+	data = np.loadtxt(guideFile)
+	xmin = data[0]
+	xmax = data[1]
+	ymin = data[2]
+	ymax = data[3]
+	zmin = data[4]
+	zmax = data[5]
+	dx   = data[6]
+	dy   = data[7]
+	amax = data[8]
+	
+	# Process snapshot files
+	dt = np.dtype([('particle',[('x', '<f4'),('y', '<f4'),('z', '<f4'),('q', '<f4'),('t', '<f4')])])
+	for snap in sorted(snaps):
+	
+		# Form output image name
+		snapName  = os.path.basename(snap).replace('csv', 'png')
+		imageName = os.path.join(imagesPath, snapName)
+		print("Processing " + snap)
+		
+		# Get data from snapshot, and retrieve particles coordinates from it
+		snapData = np.fromfile(snap, dtype=dt)
+		shp = snapData.shape
+		if snapData.size > 0:
+			if len(shp) > 1:
+				xp       = snapData[:,'x']
+				yp       = snapData[:,'y']
+				ap       = snapData[:,'t'] / amax
+			else:
+				xp       = []
+				yp       = []
+				ap       = []
+		else:
+			xp       = []
+			yp       = []
+			ap       = []
+		
+		# Plot data
+		fig = plt.figure()
+		ax = fig.add_subplot(111, aspect='equal')
+		levels = np.array([(0,0,0,a) for a in ap])
+		ax.scatter(xp, yp, s=0.1, color=levels)
+		plt.xlim(xmin,xmax)
+		plt.ylim(ymin,ymax)
+		plt.savefig(imageName)
+		plt.close()
+		
