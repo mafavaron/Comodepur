441dc555e6ab6c2b2754349798ea186d9022b1ab
Mauri Favaron <mafavaron@mac.com>
2019-07-09 12:33:50 +0200

    Update conc_compare.r
    Plant series reader updated.

--- src/conc_compare.r
+++ src/conc_compare.r
@@ -1,4 +1,6 @@
 get.plant.series <- function() {
-  d <- read.csv("final_impianto_series.csv")
+  d <- read.csv("final_impianto_series.csv", stringsAsFactors = FALSE)
+  d$Time.Stamp <- as.POSIXct(d$Time.Stamp, tz="UTC")
   return(d)
 }
+
