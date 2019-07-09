da1fd1e120237208e5c0ed1259528e66b8837c67
Mauri Favaron <mafavaron@mac.com>
2019-07-09 12:56:39 +0200

    Update conc_compare.r
    Function added to compare series values.

--- src/conc_compare.r
+++ src/conc_compare.r
@@ -18,6 +18,12 @@ get.series <- function() {
 }
 
 series.plot <- function(d) {
-  plot(d$Time.Stamp, d$Odor.from.Sonic.Met, log="y", type="l", col="blue", xlab="", ylab="Odore (UO/s)")
+  plot(d$Time.Stamp, d$Odor.from.Sonic.Met, type="l", col="blue", xlab="", ylab="Odor intensity (OU)")
   lines(d$Time.Stamp, d$Odor.from.Plant.Met, col="red")
+}
+
+series.comparison <- function(d) {
+  mx <- max(c(d$Odor.from.Sonic.Met, d$Odor.from.Plant.Met))
+  plot(d$Odor.from.Plant.Met, d$Odor.from.Sonic.Met, xlim=c(0,mx), ylim=c(0,mx), cex=0.2)
+  abline(0,1)
 }
